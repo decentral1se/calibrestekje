9cf78348421e4cfed7afd9155807152325d40119
Luke Murphy <lukewm@riseup.net>
2020-02-27 22:33:09 +0000

    Expose the bindings

--- calibrestekje/__init__.py
+++ calibrestekje/__init__.py
@@ -1,4 +1,25 @@
-"""calibrestekje module."""
+"""calibrestekje init module."""
+
+from calibrestekje.api import init_session  # noqa
+from calibrestekje.bindings import (  # noqa
+    Author,
+    Book,
+    BooksPluginDatum,
+    Comment,
+    ConversionOption,
+    CustomColumn,
+    Datum,
+    Feed,
+    Identifier,
+    Language,
+    LibraryId,
+    MetadataDirtied,
+    Preference,
+    Publisher,
+    Rating,
+    Series,
+    Tag,
+)
 
 try:
     import pkg_resources
